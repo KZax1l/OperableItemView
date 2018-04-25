45a3e7ce9628b1e5c31442264aaed7f3b58285c5
Zsago <3427241441@qq.com>
2018-04-25 15:39:49 +0800

    测试阴影库

--- app/src/main/java/com/kzax1l/oiv/sample/TestActivity.java
+++ app/src/main/java/com/kzax1l/oiv/sample/TestActivity.java
@@ -1,8 +1,13 @@
 package com.kzax1l.oiv.sample;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
+
+import com.wangjie.shadowviewhelper.ShadowProperty;
+import com.wangjie.shadowviewhelper.ShadowViewDrawable;
 
 /**
  * Created by Administrator on 2017-11-21.
@@ -15,5 +20,14 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ac_test);
+
+        ShadowProperty sp = new ShadowProperty()
+                .setShadowColor(0x59ff0000)
+                .setShadowDy(10)
+                .setShadowRadius(10)
+                .setShadowSide(ShadowProperty.BOTTOM);
+        ShadowViewDrawable sd = new ShadowViewDrawable(sp, Color.WHITE, 0, 0);
+        ViewCompat.setBackground(findViewById(R.id.oiv_test), sd);
+        ViewCompat.setLayerType(findViewById(R.id.oiv_test), ViewCompat.LAYER_TYPE_SOFTWARE, null);
     }
 }
